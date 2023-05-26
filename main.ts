880fe94faca1f4e073acbac681c72832409cd216
Jeremiah <jeremiahkoeiman1@gmail.com>
2023-05-26 21:31:12 +0200

    Add yellow circle to notes that have TODO as content on plugin load

--- main.ts
+++ main.ts
@@ -1,137 +1,77 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, Vault } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface IMappedFile {
+	name: string;
+	path: string;
+	content: string;
+	matches: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class ObsidianTodos extends Plugin {
+	// TODO: Check file content when plugin is initially loaded
+	// TODO: check only affected file(s) after the file(s) are modified
+	// TODO: Make width&height/color adjustible in css via settings
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	private _fileExplorerContainer: HTMLElement;
+	private _todoRegex = /TODO/gi;
 
 	async onload() {
-		await this.loadSettings();
+		const { vault, workspace } = this.app;
+		this._fileExplorerContainer =
+			workspace.getLeavesOfType("file-explorer")[0].view.containerEl;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerInterval(
+			window.setInterval(async () => {
+				const files = await this.getFilesWithMatchesAndContents(vault);
+				files.forEach((file) => {
+					const noteWrapperElement = this.geFileExplorerNoteElement(
+						file.path
+					);
+					const todoIndicator = document.createElement("div");
+					todoIndicator.classList.add(
+						"obsidian-todo-note-highlighter"
+					);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+					if (noteWrapperElement) {
+						noteWrapperElement.classList.add("relative");
+						noteWrapperElement.append(todoIndicator);
 					}
+				});
+			}, 5000)
+		);
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+	private async getFilesWithMatchesAndContents(
+		vault: Vault
+	): Promise<IMappedFile[]> {
+		const files = vault.getMarkdownFiles();
+		const mappedFiles = files.map(
+			async (file: TFile): Promise<IMappedFile> => {
+				const fileContent = await vault.cachedRead(file);
+				return {
+					name: file.name,
+					path: file.path,
+					content: fileContent,
+					matches: this.getTodoMatches(fileContent),
+				};
 			}
-		});
+		);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const promises = await Promise.all(mappedFiles);
+		return promises.filter(
+			(mappedFile: IMappedFile) => mappedFile.matches > 0
+		);
 	}
 
-	onunload() {
-
+	private getTodoMatches(fileContent: string): number {
+		return fileContent.match(this._todoRegex)?.length || 0;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	private geFileExplorerNoteElement(path: string): HTMLElement | null {
+		return this._fileExplorerContainer.querySelector(
+			`[data-path="${path}"]`
+		);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	onunload() {}
 }
